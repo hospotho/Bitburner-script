a27b651fcddd3853369e2536afa1b6bfe2c12c3a
hospotho <a155268747@gmail.com>
2022-01-18 03:16:44 +0800

    reduce if

--- start.js
+++ start.js
@@ -10,30 +10,26 @@ export async function main(ns) {
     await ns.asleep(1000 * 60 * 10)
     ns.run('buyServer.js')
     ns.tprint('buy BruteSSH.exe;buy FTPCrack.exe;buy relaySMTP.exe;buy HTTPWorm.exe;buy SQLInject.exe;buy DeepscanV1.exe;buy AutoLink.exe;')
-	var hackLv = ns.getHackingLevel()
+    var hackLv = ns.getHackingLevel()
     var resetTime = 1000 * 60 * 30
-    while (true) {
-        if (hackLv < 3000) {
-            var cHackLv = ns.getHackingLevel()
-            ns.clearLog()
-            ns.print(`HackLv:            ${hackLv}`)
-            ns.print(`CurrentHackLv:     ${cHackLv}`)
-            ns.print(`ResetTime:         ${resetTime / 1000}s`)
-            if (resetTime == 0 || cHackLv - hackLv > 50) {
-                ns.tprint('start.js starting new reset.js')
-                hackLv = cHackLv
-                resetTime = 1000 * 60 * 60
-                ns.run('reset.js')
-                while (ns.isRunning('reset.js', 'home')) {
-                    await ns.asleep(500)
-                }
-                await ns.asleep(1000 * 60 * 10)
+    while (hackLv < 3000) {
+        var cHackLv = ns.getHackingLevel()
+        ns.clearLog()
+        ns.print(`HackLv:            ${hackLv}`)
+        ns.print(`CurrentHackLv:     ${cHackLv}`)
+        ns.print(`ResetTime:         ${resetTime / 1000}s`)
+        if (resetTime == 0 || cHackLv - hackLv > 50) {
+            ns.tprint('start.js starting new reset.js')
+            hackLv = cHackLv
+            resetTime = 1000 * 60 * 60
+            ns.run('reset.js')
+            while (ns.isRunning('reset.js', 'home')) {
+                await ns.asleep(500)
             }
-            resetTime -= 1000
-        } else {
-            ns.tprint('HackLevel reach 3000, script exit')
-            ns.exit()
+            await ns.asleep(1000 * 60 * 10)
         }
+        resetTime -= 1000
         await ns.sleep(1000)
     }
+    ns.tprint('HackLevel reach 3000, script exit')
 }
