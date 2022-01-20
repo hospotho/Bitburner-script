e0cf602123ecea288ba6e8d1bae520fa0d3216e3
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-01-20 11:48:39 -0700

    fix L36 needed await

--- init.js
+++ init.js
@@ -33,7 +33,7 @@ export async function main(ns) {
 		while (diff > 0) {
 			var sleepTime = ns.getHackTime(_target)
 			var homeWT = Math.min(Math.ceil(diff / perCoreW), getHomeT())
-			ns.exec('w.js', 'home', homeWT, _target, 'weak')
+			await ns.exec('w.js', 'home', homeWT, _target, 'weak')
 			diff -= homeWT * perCoreW
 
 			var serverWT = Math.min(Math.ceil(diff / perServerW), getServerT())
